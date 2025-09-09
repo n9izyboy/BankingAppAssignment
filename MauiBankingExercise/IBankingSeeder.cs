3a9eb28de745c6ff2aa8ac8906119827428a46d7
n9izyboy <silvanocartinio@gmail.com>
2025-09-09 10:38:01 +0200

    Added an interface

--- MauiBankingExercise/IBankingSeeder.cs
+++ MauiBankingExercise/IBankingSeeder.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MauiBankingExercise.ViewModels
+namespace MauiBankingExercise
 {
-    class DataBaseService
+    internal interface IBankingSeeder
     {
     }
 }
